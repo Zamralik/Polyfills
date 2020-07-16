e2a4ebe87ddda689ef7b2e2ff1840a5aacf99fd6
b.blum <b.blum@agence-senzo.fr>
2020-07-16 12:09:39 +0200

    Move debugFill to debug.js

--- debug.js
+++ debug.js
@@ -78,3 +78,62 @@ function publish(root, name, module)
 		}
 	);
 }
+/* ******************************************************** */
+publish(
+	HTMLFormElement.prototype,
+	"debugFill",
+	function (data)
+	{
+		this.getFieldNames().forEach(
+			function (name)
+			{
+				const element = this.elements.namedItem(name);
+
+				if (element.hidden || element.readOnly)
+				{
+					return;
+				}
+
+				if (element instanceof HTMLInputElement)
+				{
+					if (element.type === "checkbox" || element.type === "radio")
+					{
+						element.checked = true;
+					}
+					else
+					{
+						element.value = (data && data[name]) || element.value || element.defaultValue || name;
+					}
+				}
+				else if (element instanceof HTMLTextAreaElement)
+				{
+					element.value = data && data[name] || (name + " - Lorem ipsum dolor sit amet");
+				}
+				else if (element instanceof HTMLSelectElement && element.selectedIndex > 0)
+				{
+					if (data && typeof data[name] === "number" && data[name] < element.options.length)
+					{
+						element.selectedIndex = data[name];
+					}
+					else
+					{
+						if (data && typeof data[name] === "string")
+						{
+							element.value = data[name];
+						}
+
+						if (element.selectedIndex < 1)
+						{
+							element.selectedIndex = (element.options.length - 1);
+						}
+					}
+				}
+				else if (element instanceof RadioNodeList)
+				{
+					element[0].checked = true;
+				}
+			},
+			this
+		);
+	}
+);
